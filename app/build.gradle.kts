d27ba1d16f2830f0e0a5cf911309515912f912e0
infinite-dev22 <mwigojm@gmail.com>
2025-01-02 01:40:25 +0300

    chore: Update dependencies and project setup
    This commit updates the project dependencies, applies code style improvements, and refactors the project structure.
    
    The following changes were made:
    
    - Updated Android Gradle Plugin (AGP) to `8.8.0-rc02`.
    - Updated Kotlin to `2.1.0`.
    - Updated Core KTX to `1.15.0`.
    - Updated JUnit to `4.13.2`.
    - Updated JUnit version to `1.2.1`.
    - Updated Espresso Core to `3.6.1`.
    - Updated Lifecycle Runtime KTX to `2.8.7`.
    - Updated Activity Compose to `1.9.3`.
    - Updated Compose BOM to `2024.12.01`.
    - Updated Compose Navigation to `2.8.5`.
    - Added Kotlin Serialization dependency (`1.7.3`).
    - Added Glide Image dependency (`1.0.0-beta01`).
    - Added Material Icons dependency (`1.7.6`).
    - Added Material UI dependency (`1.7.6`).
    - Removed old theme, color, and type definitions.
    - Updated application ID and namespace to `inc.nomard.nomard_hotel_booking_app_jetpack_compose`.
    - Applied Kotlin Serialization plugin.
    - Updated build configuration for dependencies.
    - Removed old test files.
    - Removed old UI code.
    - Removed old theme code.
    - Removed old feature package structure.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
-    namespace = "com.example.nomard_hotel_booking_app_jetpack_compose"
+    namespace = "inc.nomard.nomard_hotel_booking_app_jetpack_compose"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.nomard_hotel_booking_app_jetpack_compose"
+        applicationId = "inc.nomard.nomard_hotel_booking_app_jetpack_compose"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
@@ -49,6 +50,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.glide.image)
+    implementation(libs.androidx.material.icons)
+    implementation(libs.androidx.material)
+//    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
